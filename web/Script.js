88b2b5f29b2260a5a48c92fa35dbd387ffe07d28
Domantas-L <lopatovasd878@gmail.com>
2026-07-30 15:10:39 +0300

    Feat: wired api to frontend, added delete button functionality

--- web/Script.js
+++ web/Script.js
@@ -1,20 +1,3 @@
-
-let TodayMockData = [
-    {
-        Done: true,
-        id: 1,
-        name: "Open github",
-        Start: "12:45",
-        End: "14:45"
-    },
-    {
-        Done: false,
-        id: 2,
-        name: "Close github",
-        Start: "14:45",
-        End: "14:50"
-    },
-];
 const today_list = document.querySelector("#Today-List");
 const form_overlay = document.querySelector("#form-overlay");
 const task_form = document.querySelector("#task_form");
@@ -22,7 +5,53 @@ const form_open_btn = document.querySelector("#open-form-btn");
 const form_close_btn = document.querySelector("#Close-form-btn");
 const Add_new_list = document.querySelector("#Board");
 const item_delete_btn=document.querySelector("#Item_delete_btn");
+const task_counter = document.querySelector("#Task-counter");
+const API = "http://localhost:3001";
 
+init();
+
+async function init() {
+    showLoading();
+    try{
+    const TodayTasks = await LoadTodayList();
+    RenderTodayTask(TodayTasks);}
+    catch (err)
+    {
+        showError("Couldn't load workouts. Is the API running?")
+    }
+
+}
+
+async function LoadTodayList() {    
+    const res = await fetch(`${API}/Tasks`);
+    if(!res.ok)
+    {
+        throw new Error("Failed to load");
+    }
+    const body = await res.json();
+    return body.data;
+}
+
+async function createTodayTask(name,start,end) {
+    const res = await fetch(`${API}/Tasks`,
+        {
+            method: "POST",
+            headers:{ "Content-Type": "application/json" },
+            body:  JSON.stringify({ name, Start: start, End: end }),
+        });
+        if(!res.ok) throw new Error ("Failed to create");
+        return res.json();
+}
+
+async function DeleteTodayTask(id) {
+    const res = await fetch(`${API}/Tasks/${id}`,{
+        method:"DELETE",
+    });
+    if (!res.ok) {
+    throw new Error("Failed to delete task");
+  }
+  return res.json();
+}
 
 form_open_btn.addEventListener("click",() =>{
     form_overlay.classList.remove("hidden");
@@ -34,72 +63,54 @@ form_close_btn.addEventListener("click",()=>
     form_overlay.classList.add("hidden");
     task_form.reset();
 });
-function AddTodayTask(task)
-{
-    const newData = {
-    id: Date.now(),
-    name: document.querySelector("#Task_name").value,
-    Start: document.querySelector("#Task_Start_time").value,
-    End: document.querySelector("#Task_End_time").value,
-    Done: false
-    };
-    TodayMockData.push(newData);
-    return newData;
-}
-task_form.addEventListener("submit",(event)=>
+task_form.addEventListener("submit", async (event)=>
 {
     event.preventDefault();
 
-   AddTodayTask(
-    {
-    name: document.querySelector("#Task_name").value,
-    Start: document.querySelector("#Task_Start_time").value,
-    End: document.querySelector("#Task_End_time").value
-    }
-   );
+    const name = document.querySelector("#Task_name").value;
+    const start = document.querySelector("#Task_Start_time").value;
+    const end = document.querySelector("#Task_End_time").value;
 
-    RenderTodayTask();
+  try{
+    await createTodayTask(name,start,end);
+    const tasks = await LoadTodayList();
+    RenderTodayTask(tasks);
 
-    form_overlay.classList.add("hidden");
+    form_overlay.classList("hidden");
     task_form.reset();
+  }
+  catch (err){
+    console.error("Error creating task:", err);
+  }
+
 });
 
-function getTodayTask()
-{
-    if (TodayMockData.length === 0) {
-        console.log("The list is empty")
-        return [];
-    }
-    return TodayMockData;
-}
-
-function deleteTodayTask(id)
-{
-    const index = TodayMockData.find(task => task.id ===id);
-    if(index != -1)
-    {
-        TodayMockData.splice(index,1);
-    }
-}
-
-today_list.addEventListener("click",(event)=>
+today_list.addEventListener("click", async(event)=>
 {
     const deleteBtn = event.target.closest(".delete-btn");
-    if(!deleteBtn)
-        return;
-    const id = Number(deleteBtn.id);
-    deleteTodayTask(id);
-    RenderTodayTask();
-})
 
-RenderTodayTask();
+    const id = Number(deleteBtn.dataset.id);
+    try{
+       await DeleteTodayTask(id);
+       const updatedlist = LoadTodayList();
+       await RenderTodayTask(updatedlist);
+    }
+    catch (err)
+    {
+        console.error("Error deleting task:", err);
+    }
+});
+function RenderTodayTask(tasks = []) {
 
-function RenderTodayTask() {
-    if (TodayMockData.length == 0) {
+    if(task_counter)
+    {
+        task_counter.textContent = tasks.length;
+    }
+    if (!tasks || tasks.length == 0) {
         today_list.innerHTML = "<li><p>Nothing was added yet</p></li>";
         return;
     }
-    today_list.innerHTML = TodayMockData.map(w =>
+    today_list.innerHTML = tasks.map(w =>
     `<li class="list-item">
       <label for=" task-check-box">
             <input type="checkbox" class="List-checkbox" id="Task-${w.id}" ${w.Done ? "checked" : ""}>
@@ -121,5 +132,11 @@ function RenderTodayTask() {
     ).join("");
     console.log("This works");
 }
-
-RenderTodayTask();
+function showLoading()
+{
+    today_list.innerHTML ="<p class='loading'>Loading Tasks…</p>";
+}
+function showError(message)
+{
+    today_list.innerHTML =`<p class='error'>${message}}</p>`;
+}
